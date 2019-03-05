8a6a831387e6955663dfe54d2c1f0c68fd5580dd
ren0306 <h1201815026@hiratagakuen.onmicrosoft.com>
2019-03-05 15:33:47 +0900

    asfbefgbfen

--- Star Regein/Star Regein/GameHead.h
+++ Star Regein/Star Regein/GameHead.h
@@ -9,6 +9,7 @@ enum OBJ_NAME
 	OBJ_TITLE,
 
 	OBJ_HERO,
+	OBJ_STAGECHOICE,
 };
 //------------------------------------------------
 
@@ -53,7 +54,6 @@ struct UserData
 
 //ゲームシーンオブジェクトヘッダ------------------
 #include "ObjHero.h"
-
 #include "ObjTitle.h"
 #include "ObjStageChoice.h"
 //------------------------------------------------
@@ -66,6 +66,5 @@ struct UserData
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-//#define SET_GAME_START  CSceneTitle
-#define SET_GAME_START  CSceneMain
+#define SET_GAME_START  CSceneStageChoice
 //-----------------------------------------------
