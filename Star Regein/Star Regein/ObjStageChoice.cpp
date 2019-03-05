cdbcf6970291b8b0cad0e9dc3b19d5a0ba4939e2
ren0306 <h1201815026@hiratagakuen.onmicrosoft.com>
2019-03-05 11:06:34 +0900

    as

---
+++ Star Regein/Star Regein/ObjStageChoice.cpp
@@ -0,0 +1,92 @@
+//使用するヘッダー
+#include "GameL\DrawTexture.h"
+#include "GameL\WinInputs.h"
+#include "GameL\DrawFont.h"
+#include "GameL\SceneManager.h"
+#include "GameL/Audio.h"
+
+#include "GameHead.h"
+#include "ObjStageChoice.h"
+
+
+
+
+//使用するネームスペース
+using namespace GameL;
+
+
+//イニシャライズ
+void CObjStageChoice::Init()
+{
+	m_up = false;
+	m_down = false;
+	m_start = false;
+	m_end = false;
+
+
+}
+
+//アクション
+void CObjStageChoice::Action()
+{
+}
+
+//ドロー
+void CObjStageChoice::Draw()
+{
+	float c[4] = { 1,1,1,1 };
+	//上キーを押したとき
+	if (Input::GetVKey(VK_UP) == true)
+	{
+		m_up = true;
+		m_down = false;
+
+	}
+	//下キーを押したとき
+	if (Input::GetVKey(VK_DOWN) == true)
+	{
+		m_down = true;
+		m_up = false;
+	}
+	//ゲーム開始
+	if (m_start == true)
+	{
+		;
+	}
+	//ゲーム終了
+	if (m_end == true)
+	{
+		exit(4);
+	}
+	//カーソルが動く
+	if (m_up == true)
+	{
+		Font::StrDraw(L"→ゲームスタート", 280, 300, 32, c);
+		Font::StrDraw(L"ゲーム終了", 280, 340, 32, c);
+		//Ｚキーで始める
+		if (Input::GetVKey('Z') == true)
+		{
+			m_start = true;
+		}
+
+	}
+	else if (m_down == true)
+	{
+		Font::StrDraw(L"ゲームスタート", 280, 300, 32, c);
+		Font::StrDraw(L"→ゲーム終了", 280, 340, 32, c);
+		if (Input::GetVKey('Z') == true)
+		{
+			m_end = true;
+		}
+
+
+	}
+	//始めはこの状態
+	else
+	{
+		Font::StrDraw(L"ゲームスタート", 280, 300, 32, c);
+		Font::StrDraw(L"ゲーム終了", 280, 340, 32, c);
+	}
+
+
+}
---
+++ Star Regein/Star Regein/SceneStageChoice.cpp
@@ -0,0 +1,41 @@
+//STLデバッグ機能をOFFにする
+#define _SECURE_SCL	(0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include "GameL\DrawFont.h"
+#include "GameL/DrawTexture.h"
+#include "GameL\Audio.h"
+
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include "GameHead.h"
+#include "SceneStageChoice.h"
+
+//コンストラクタ
+CSceneStageChoice::CSceneStageChoice()
+{
+
+}
+
+//デストラクタ
+CSceneStageChoice::~CSceneStageChoice()
+{
+
+}
+
+//ゲームタイトル初期化メソッド
+void CSceneStageChoice::InitScene()
+{
+
+}
+
+//ゲームタイトル実行中メソッド
+void CSceneStageChoice::Scene()
+{
+
+}
